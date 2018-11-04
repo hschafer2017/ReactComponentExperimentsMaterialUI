869aff24d4c0eca334d0dbd88675e73ac52920e1
hschafer2017 <hschafer@umich.edu>
2018-11-04 14:49:17 +0000

    initial material ui button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import '../libraries/bootstrap.min.css'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <CssBaseline />
+      <div className="body">
+        <header className="">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            Let's try to make a react app. 
           </p>
           <a
             className="App-link"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+function AppButton() {
+    return (
+        <React.Fragment>
+        <CssBaseline />
+      <Button variant="contained" color="primary">
+        This is my button
+      </Button>
+      </React.Fragment>
+    );
+  }
+
+ReactDOM.render(<AppButton />, document.getElementById('root'));
+
+
+
+  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// ReactDOM.render(<App />, document.querySelector('#app'));
