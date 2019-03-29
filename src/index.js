bb2d9f411d07f0521c6d404f318292cf879cba45
hschafer2017 <hschafer@umich.edu>
2019-03-28 22:44:52 -0400

    experiment with other buttons and material ui components

--- src/index.js
+++ src/index.js
@@ -5,14 +5,36 @@ import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import * as serviceWorker from './serviceWorker';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
+const inputProps = {
+  step: 500,
+};
+
+// function AppNav() {
+//  return () {
+//   <React.Fragment>
+//     <CssBaseline />
+
+//     </React.Fragment>
+//  }
+// }
+
+// ReactDOM.render(<AppNav />, document.getElementById('nav'));
 
 function AppButton() {
     return (
         <React.Fragment>
         <CssBaseline />
-      <Button variant="contained" color="primary">
-        This is my button
+      <Button variant="contained" color="secondary">
+        This is my button <i class="material-icons">
+        accessibility
+        </i>
+      </Button>
+      <Button variant="outlined" color="primary" size="small">
+      This is my other button<i class="material-icons">
+        done
+        </i>
       </Button>
       </React.Fragment>
     );
@@ -20,6 +42,34 @@ function AppButton() {
 
 ReactDOM.render(<AppButton />, document.getElementById('root'));
 
+function AppFeature() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+        <Button variant="contained" color="primary">
+        App Feature Button
+          </Button>
+    </React.Fragment>
+  )
+}
+ReactDOM.render(<AppFeature />, document.getElementById('search'));
+
+
+function AppForm() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+          <TextField id="time" type="text" label='username' inputProps={inputProps} />
+          <TextField id="time" type="password" label='password' inputProps={inputProps} />
+          <Button variant="fab" color="secondary" size='regular'>
+          <i class="material-icons">
+        done
+        </i>
+          </Button>
+    </React.Fragment>
+  )
+}
+ReactDOM.render(<AppForm />, document.getElementById('loginForm'));
 
 
   
