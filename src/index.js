e77f817a22534b4d10d705d4ac15ee90c972077d
hschafer2017 <hschafer@umich.edu>
2019-04-01 21:45:58 -0700

    added simple navbar to landing page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {AppBar, Tabs, Tab, Typography, Toolbar} from '@material-ui/core'
 import './libraries/bootstrap.min.css';
 import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import * as serviceWorker from './serviceWorker';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
 const inputProps = {
   step: 500,
 };
 
-// function AppNav() {
-//  return () {
-//   <React.Fragment>
-//     <CssBaseline />
+const nav = {
+    background: 'royalblue',
+    color: '#fafafa',
+    display: 'block',
+    width: '100%',
+    flexGrow: 1,
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 30,
+      },
+    root: {
+        flexGrow: 1,
+      },
+    grow: {
+        flexGrow: 1,
+      },
+}
 
-//     </React.Fragment>
-//  }
-// }
 
-// ReactDOM.render(<AppNav />, document.getElementById('nav'));
+class Nav extends React.Component {
+  render() {
+    return (
+        <AppBar title="Let's Try Cool Styles!" style={nav} className={nav.root}>
+        <Toolbar>
+            <IconButton className={nav.menuButton} color="inherit" aria-label="Open drawer">
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap className={nav.grow}>
+                Try Cool Styles!
+            </Typography>
+            <Tabs>
+                <Tab label="Home" />
+                <Tab label="About" />
+                <Tab label="More" />
+                <Tab label="Contact" />
+            </Tabs>
+            </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+ReactDOM.render(<Nav />, document.getElementById('nav'))
 
 function AppButton() {
     return (
