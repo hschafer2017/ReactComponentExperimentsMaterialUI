745bac2fa147daa17077694d3f3b6d1962c17c1b
hschafer2017 <hschafer@umich.edu>
2019-04-03 22:56:58 -0700

    added html and photo source setup for photo grid

---
+++ src/tileData.js
@@ -0,0 +1,32 @@
+const tileData = [
+    {
+        img: 'https://images.pexels.com/photos/2055231/pexels-photo-2055231.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: 'https://images.pexels.com/photos/1095901/pexels-photo-1095901.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        title: 'Image',
+        author: 'author',
+        cols: 1,
+    },
+    {
+        img: "https://images.pexels.com/photos/962986/pexels-photo-962986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: 'Image',
+        author: 'author',
+        cols: 3,
+    },
+    {
+        img: "https://images.pexels.com/photos/1050316/pexels-photo-1050316.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: 'Image',
+        author: 'author',
+        cols: 1,
+    },
+    {
+        img: "https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    ];
